e9256eff75935d31ea6acb9102fa52ca371872ad
Teja36 <bommarajutej@gmail.com>
2021-11-14 19:17:50 +0530

    Added dark mode

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grid, Typography } from "@mui/material";
+import { Container, Grid, Paper, Typography } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { db } from "../firebase";
 import { collection, where, query, onSnapshot } from "firebase/firestore";
 
@@ -9,47 +10,69 @@ import AddNote from "./AddNote";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
+  const [darkMode, setDarkMode] = useState(false);
 
   const uid = localStorage.getItem("uid");
   const q = query(collection(db, "notes"), where("userId", "==", uid));
 
-  useEffect(() =>
-    onSnapshot(q, (snapshot) => {
-      setNotes(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          title: doc.data().title,
-          desc: doc.data().description,
-        }))
-      );
-    })
+  useEffect(
+    () =>
+      onSnapshot(q, (snapshot) => {
+        setNotes(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            title: doc.data().title,
+            desc: doc.data().description,
+          }))
+        );
+        console.log("snapshot");
+      }),
+    []
   );
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  const lightTheme = createTheme({
+    palette: {
+      mode: "light",
+    },
+  });
+
   return (
-    <>
-      <Navbar />
-      <Container>
-        <Grid container spacing={3}>
-          {notes.length > 0 ? (
-            notes.map((data) => {
-              return (
-                <Grid item md={4} sm={6} xs={12} key={data.id}>
-                  <NoteCard id={data.id} title={data.title} desc={data.desc} />
-                </Grid>
-              );
-            })
-          ) : (
-            <Typography
-              variant="h1"
-              component="p"
-              sx={{ textAlign: "center", flexGrow: 1, color: "#9e9e9e" }}
-            >
-              Start adding your notes here.
-            </Typography>
-          )}
-        </Grid>
-        <AddNote />
-      </Container>
-    </>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Paper style={{ height: "100vh" }}>
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Container>
+          <Grid container spacing={3}>
+            {notes.length > 0 ? (
+              notes.map((data) => {
+                return (
+                  <Grid item md={4} sm={6} xs={12} key={data.id}>
+                    <NoteCard
+                      id={data.id}
+                      title={data.title}
+                      desc={data.desc}
+                    />
+                  </Grid>
+                );
+              })
+            ) : (
+              <Typography
+                variant="h1"
+                component="p"
+                sx={{ textAlign: "center", flexGrow: 1, color: "#9e9e9e" }}
+              >
+                Start adding your notes here.
+              </Typography>
+            )}
+          </Grid>
+          <AddNote />
+        </Container>
+      </Paper>
+    </ThemeProvider>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Button,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
 
-export default function Navbar() {
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+
+export default function Navbar({ darkMode, setDarkMode }) {
   let navigate = useNavigate();
 
   const handleClick = () => {
     navigate("/login");
+  };
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode);
   };
 
   const handleLogout = () => {
@@ -17,13 +31,29 @@ export default function Navbar() {
   return (
     <AppBar position="sticky" sx={{ mb: 5 }}>
       <Toolbar>
-        <Typography variant="h6">Notes App</Typography>
+        <Typography variant="h6" sx={{ mr: "auto" }}>
+          Notes App
+        </Typography>
+        {darkMode ? (
+          <Tooltip title="Light mode">
+            <IconButton aria-label="light-mode" onClick={handleDarkMode}>
+              <LightModeIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Dark mode">
+            <IconButton aria-label="dark-mode" onClick={handleDarkMode}>
+              <DarkModeIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+
         {localStorage.getItem("uid") === null ? (
-          <Button onClick={handleClick} color="inherit" sx={{ ml: "auto" }}>
+          <Button onClick={handleClick} color="inherit">
             Login
           </Button>
         ) : (
-          <Button onClick={handleLogout} color="inherit" sx={{ ml: "auto" }}>
+          <Button onClick={handleLogout} color="inherit">
             Logout
           </Button>
         )}
